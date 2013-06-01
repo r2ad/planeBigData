f1e2f2875d168aba44803299d027d8b91cbd7a7d
vlad <vlad@v-lad.org>
2013-06-01 13:58:39 -0400

    Added executable ...

---
+++ ingest/src/main/java/Convert.java
@@ -0,0 +1,14 @@
+/**
+ * User: vlad
+ * Created : 6/1/13 1:57 PM
+ */
+public class Convert {
+
+
+    public static  void main ( String args [] ) {
+
+        System.out.println("We don't have anything here yet..");
+
+    }
+
+}
