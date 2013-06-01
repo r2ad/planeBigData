13e0771e09570e861353a9ea061df344db14290d
vlad <vlad@v-lad.org>
2013-06-01 18:03:09 -0400

    Recovered from merge conflict...

--- ingest/src/test/java/LoadFileTest.java
+++ ingest/src/test/java/LoadFileTest.java
@@ -9,6 +9,8 @@ import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
 import java.io.IOException;
 
+import static junit.framework.Assert.assertEquals;
+
 /**
  * User: vlad
  * Created : 6/1/13 2:23 PM
@@ -19,24 +21,26 @@ public class LoadFileTest {
 
     @Before
     public void setup() throws SAXException, ParserConfigurationException {
-        final File indir = new File("../data/IN-kml");
-        final File outdir = new File("../data/OUT-json");
         final SAXParserFactory spf = SAXParserFactory.newInstance();
         spf.setNamespaceAware(true);
         sp = spf.newSAXParser();
     }
 
-    @Test
-    public void doesEverythingWork() throws IOException, SAXException {
-        KML2TSV me = new KML2TSV(new File("."),new File( "."), sp, true);
-        me.workFiles();
-    }
 
     @Test
     public void processSingleFile() throws IOException, SAXException {
-        KML2TSV me = new KML2TSV(new File("."),new File( "."), sp, true);
+        KML2TSV me = new KML2TSV(sp);
         final File file = new File(this.getClass().getClassLoader().getResource("plane.kml").getFile());
         me.acceptInputFile(file);
+
+        assertEquals(me.getFlightNum(), "JBU1732");
+        assertEquals(me.getHeading(), 201);
+        assertEquals(me.getDepartTime(), "06/01/2013 10:16 AM EDT (1416Z)");
+        assertEquals(me.getLattitude(), 40.66, 0.1);
+        assertEquals(me.getLongitude(), -73.75, .01);
+        assertEquals(me.getAltitude(), 122.0, .01);
+
+
     }
 
 }
