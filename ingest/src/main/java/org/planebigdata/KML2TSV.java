3393ae51739cd5ff6ef21956dacab81f2a721b6a
vlad <vlad@v-lad.org>
2013-06-02 07:59:11 -0400

    Code clean up and adjusted build scripts.

--- ingest/src/main/java/org/planebigdata/KML2TSV.java
+++ ingest/src/main/java/org/planebigdata/KML2TSV.java
@@ -77,7 +77,7 @@ public class KML2TSV extends DefaultHandler {
     private String DepartTime = "";
     private int heading = 0;
 
-    final public Coordinates getCoordinates(int idx) {
+    final public Coordinates getCoordinates(final int idx) {
         return coordList.get(idx);
     }
 
@@ -117,7 +117,7 @@ public class KML2TSV extends DefaultHandler {
     }
 
     @Override
-    final public void characters(char[] ch, int start, int length) throws SAXException {
+    final public void characters(final char[] ch,final  int start,final  int length) throws SAXException {
         if (chars != null) {
             chars.append(ch, start, length);
         }
@@ -137,7 +137,7 @@ public class KML2TSV extends DefaultHandler {
     }
 
     @Override
-    final public void endElement(String uri, String lName, String qName) throws SAXException {
+    final public void endElement(final String uri,final  String lName,final  String qName) throws SAXException {
         final String content = doneChars();
 
 
@@ -154,7 +154,7 @@ public class KML2TSV extends DefaultHandler {
 
     }
 
-    final public void parseCoordinates(String content) throws SAXException {
+    final public void parseCoordinates(final String content) throws SAXException {
         String[] coords = COORD_SPLIT_REGEX.split(content);
         for (String coord : coords) {
             if (coord.trim().length() == 0) {
@@ -164,7 +164,7 @@ public class KML2TSV extends DefaultHandler {
         }
     }
 
-    final private String extractDepartTime(String content) throws SAXException {
+    final private String extractDepartTime(final String content) throws SAXException {
         final Matcher matcher = DEPART_TIME_REGEX.matcher(content.trim());
         if (!matcher.matches()) {
             throw new SAXException("Problem parsing departure time string " + content + " doesn't match");
@@ -179,7 +179,7 @@ public class KML2TSV extends DefaultHandler {
 
 
 
-    final public void extractCoordinates(String longLat) throws SAXException {
+    final public void extractCoordinates(final String longLat) throws SAXException {
         final Matcher matcher = LONGLAT_REGEX.matcher(longLat);
         //ignore non matching and return null >> this erases all polygon shapes for the moment
         if (!matcher.matches()) {
