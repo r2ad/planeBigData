1f720be89c365b285cc6decaa68f0010f75aae66
vlad <vlad@v-lad.org>
2013-06-02 17:04:19 -0400

    Added things..

--- ingest/src/main/java/org/planebigdata/KML2TSV.java
+++ ingest/src/main/java/org/planebigdata/KML2TSV.java
@@ -1,7 +1,7 @@
 package org.planebigdata;
 /*
  * Store KML ouput to a tab separated file:
- * FlightNum  DepartTime  Heading Lat Long Alt
+ * flightNum  departTime  Heading Lat Long Alt
  * Note: cdata includes Departed: 06/01/2013 10:16 AM EDT (1416Z)
  */
 
@@ -26,13 +26,13 @@ public class KML2TSV extends DefaultHandler {
 
     private ArrayList<Coordinates> coordList = new ArrayList<Coordinates>(10);
 
-    public KML2TSV(final SAXParser sp, PrintWriter out) {
+    public KML2TSV(final SAXParser sp, final PrintWriter out) {
         parser = sp;
         output = out;
     }
 
 
-    public void acceptInputFile(final File srcFile) throws SAXException, IOException {
+    final  public void acceptInputFile(final File srcFile) throws SAXException, IOException {
         System.out.println(String.format("starting on file %s", srcFile.getAbsolutePath()));
         try {
             outputHeader();
@@ -54,12 +54,8 @@ public class KML2TSV extends DefaultHandler {
 
 
     private void doneOutput() throws IOException {
-        try {
-            //TODO: Do output here..
-        } finally {
-            IOUtils.closeQuietly(output);
-            output = null;
-        }
+        IOUtils.closeQuietly(output);
+        output = null;
     }
 
     // For flight data:
@@ -67,39 +63,38 @@ public class KML2TSV extends DefaultHandler {
     * ref: Lat/Lon ref: http://code.google.com/p/simplelatlng/source/browse/src/main/java/com/javadocmd/simplelatlng/LatLng.java
     */
 
-    public String getFlightNum() {
-        return FlightNum;
+    public final String getFlightNum() {
+        return flightNum;
     }
 
-    private String FlightNum = "";
+    private String flightNum = "";
 
-    public int getHeading() {
+    public final int getHeading() {
         return heading;
     }
 
 
-    public String getDepartTime() {
-        return DepartTime;
+    public final String getDepartTime() {
+        return departTime;
     }
 
-    private String DepartTime = "";
+    private String departTime = "";
     private int heading = 0;
 
-    public Coordinates getCoordinates(int idx) {
+    public final Coordinates getCoordinates(final int idx) {
         return coordList.get(idx);
     }
 
-    public int getCoordinatesCount() {
+    public final int getCoordinatesCount() {
         return coordList.size();
     }
 
-    public class Coordinates {
+    static public class Coordinates {
         public double longitude = 0.0;
         public double lattitude = 0.0;
         public double altitude = 0.0;
     }
 
-    boolean parsingFirstHalf = true;
 
     private static final String KML_NS = "http://earth.google.com/kml/2.2";
     private static final String NAME_ELM = "name";
@@ -128,14 +123,14 @@ public class KML2TSV extends DefaultHandler {
     }
 
     @Override
-    public void characters(char[] ch, int start, int length) throws SAXException {
+    public final void characters(final char[] ch, final int start, final int length) throws SAXException {
         if (chars != null) {
             chars.append(ch, start, length);
         }
     }
 
     @Override
-    final public void startElement(final String uri, final String lName, final String qName, final Attributes atts)
+    public final void startElement(final String uri, final String lName, final String qName, final Attributes atts)
             throws SAXException {
         if (!KML_NS.equals(uri)) {
             return;
@@ -148,46 +143,44 @@ public class KML2TSV extends DefaultHandler {
     }
 
     @Override
-    public void endElement(String uri, String lName, String qName) throws SAXException {
+    public final void endElement(final String uri,final  String lName, final String qName) throws SAXException {
         final String content = doneChars();
 
 
         if (HEADING_ELM.equals(lName)) {
             heading = Integer.parseInt(content);
         } else if (DESCR_ELM.equals(lName)) {
-            DepartTime = extractDepartTime(content);
+            departTime = extractDepartTime(content);
         } else if (COORD_ELM.equals(lName)) {
             parseCoordinates(content);
         } else if (NAME_ELM.equals(lName)) {
-            FlightNum = content;
-        } else if (PLACE_ELM.equals(lName)) {
-            if (coordList.size() > 1) {
-                // Dump out the records
-                boolean firstTime = true;
-                for (Coordinates coord : coordList) {
+            flightNum = content;
+        } else if (PLACE_ELM.equals(lName) && coordList.size() > 1) {
+            // Dump out the records
+            boolean firstTime = true;
+            for (Coordinates coord : coordList) {
 
-                    output.print(FlightNum + "\t");
-                    output.print(DepartTime + "\t");
+                output.print(flightNum + "\t");
+                output.print(departTime + "\t");
 
-                    if ( firstTime ) {
-                        output.print(heading + "\t");
-                    }  else {
-                        output.print("\t\t");
-                    }
-                    output.print(coord.lattitude + "\t");
-                    output.print(coord.longitude + "\t");
-                    output.print(coord.altitude  );
-                    output.println("");
-                    firstTime = false;
+                if (firstTime) {
+                    output.print(heading + "\t");
+                } else {
+                    output.print("\t\t");
                 }
-
+                output.print(coord.lattitude + "\t");
+                output.print(coord.longitude + "\t");
+                output.print(coord.altitude);
+                output.println("");
+                firstTime = false;
             }
+
         }
 
 
     }
 
-    public void parseCoordinates(String content) throws SAXException {
+    public final void parseCoordinates(final String content) throws SAXException {
         String[] coords = COORD_SPLIT_REGEX.split(content);
         for (String coord : coords) {
             if (coord.trim().length() == 0) {
@@ -197,7 +190,7 @@ public class KML2TSV extends DefaultHandler {
         }
     }
 
-    private String extractDepartTime(String content) throws SAXException {
+    private String extractDepartTime(final String content) throws SAXException {
         final Matcher matcher = DEPART_TIME_REGEX.matcher(content.trim());
         if (!matcher.matches()) {
             throw new SAXException("Problem parsing departure time string " + content + " doesn't match");
@@ -210,7 +203,7 @@ public class KML2TSV extends DefaultHandler {
     private static final Pattern DEPART_TIME_REGEX = Pattern.compile("^.*<tr><td>Departed: (.+Z\\))</td>.*$");
 
 
-    public void extractCoordinates(String longLat) throws SAXException {
+    public final void extractCoordinates(final String longLat) throws SAXException {
         final Matcher matcher = LONGLAT_REGEX.matcher(longLat);
         //ignore non matching and return null >> this erases all polygon shapes for the moment
         if (!matcher.matches()) {
