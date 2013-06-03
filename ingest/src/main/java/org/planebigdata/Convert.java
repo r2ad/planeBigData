b1341832874191511d2d919b20c2ee2cd02388cd
vlad <vlad@v-lad.org>
2013-06-02 21:06:46 -0400

    Implemented missing methods for conversion.

--- ingest/src/main/java/org/planebigdata/Convert.java
+++ ingest/src/main/java/org/planebigdata/Convert.java
@@ -17,14 +17,27 @@ public class Convert {
         if ( args.length == 1 )
         {
             ConvertFile(args[0],System.out);
+        }  else if ( args.length == 2 )
+        {
+            ConvertFile(args[0],args[1]);
         }
 
     }
 
-    public static void ConvertFile(String file, PrintStream out) throws SAXException, ParserConfigurationException, IOException {
+    private static void ConvertFile(String inFile, String outFile) throws IOException, SAXException, ParserConfigurationException {
+        ConvertFile(inFile,
+                new PrintStream(
+                new FileOutputStream(outFile)));
+    }
 
+    public static void ConvertFile(String inFile, PrintStream out) throws IOException, SAXException, ParserConfigurationException {
+        InputStream in = new FileInputStream(inFile);
+        ConvertFile(in,out);
+    }
+
+    public static void ConvertFile(InputStream file, PrintStream out) throws SAXException, ParserConfigurationException, IOException {
         KML2TSV me = new KML2TSV(new PrintWriter(out));
-        me.acceptInputFile(new FileInputStream(file));
+        me.acceptInputFile(file);
 
 
     }
